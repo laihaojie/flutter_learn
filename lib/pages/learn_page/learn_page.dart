4e643fe68a794de689de45359be505dfb94d90ab
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-09-25 15:45:07 +0800

    chore: update

--- lib/pages/learn_page/learn_page.dart
+++ lib/pages/learn_page/learn_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learn/pages/learn_page/learn_controller.dart';
-import 'package:flutter_learn/pages/learn_page/learn_other_widget.dart';
+import 'package:flutter_learn/pages/learn_page/learn_widget/learn_other_widget.dart';
 import 'package:flutter_learn/pages/learn_page/learn_widget/learn_grid_view.dart';
 import 'package:flutter_learn/pages/learn_page/learn_widget/learn_list_view.dart';
 import 'package:get/get.dart';
