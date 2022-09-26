4f161ffca0cab20292b8c7aaae22790c2f27f614
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-09-26 20:42:57 +0800

    chore: update

--- lib/pages/challenge_page/challenge/first.dart
+++ lib/pages/challenge_page/challenge/first.dart
@@ -9,20 +9,49 @@ class First extends StatelessWidget {
       appBar: AppBar(
         title: const Text('旋转屏幕布局'),
       ),
+      // body: const MyAnswer(),
       body: Center(
-        child: Flex(
-          // 根据屏幕是否横屏来决定主轴方向
-          direction: MediaQuery.of(context).orientation == Orientation.portrait
-              ? Axis.vertical
-              : Axis.horizontal,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: List.generate(
-            5,
-            (index) => Container(
-              width: 100,
-              height: 100,
-              color: Colors.blue,
-            ),
+        child: OrientationBuilder(
+          builder: (context, orientation) {
+            return Flex(
+              direction: orientation == Orientation.portrait
+                  ? Axis.vertical
+                  : Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                for (int i = 0; i < 5; i++)
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue,
+                  ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class MyAnswer extends StatelessWidget {
+  const MyAnswer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Flex(
+        // 根据屏幕是否横屏来决定主轴方向
+        direction: MediaQuery.of(context).orientation == Orientation.portrait
+            ? Axis.vertical
+            : Axis.horizontal,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: List.generate(
+          5,
+          (index) => Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue,
           ),
         ),
       ),
