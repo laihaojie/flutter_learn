ba2bc3716a425f0565285e9c924116fcf06df2a9
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-09-26 17:38:20 +0800

    chore: update

--- lib/pages/learn_page/learn_widget/learn_sliver_widget.dart
+++ lib/pages/learn_page/learn_widget/learn_sliver_widget.dart
@@ -11,7 +11,7 @@ class LearnSliverWidget extends StatelessWidget {
           const SliverAppBar(
             // title: Text('SliverAppBar'),
             pinned: true, // 固定在顶部
-            expandedHeight: 500, // 展开高度
+            // expandedHeight: 500, // 展开高度
             floating: true, // 滚动时显示
             // stretch: true, // 拉伸
             snap: true, // 滚动时显示
@@ -31,17 +31,34 @@ class LearnSliverWidget extends StatelessWidget {
               ],
             ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) => Container(
-                color: Colors.white,
-                height: 150,
-                child: Center(
-                  child: Text('item $index'),
-                ),
-              ),
-              childCount: 20,
+          // SliverList(
+          //   delegate: SliverChildBuilderDelegate(
+          //     (context, index) => Container(
+          //       color: Colors.white,
+          //       height: 150,
+          //       child: Center(
+          //         child: Text('item $index'),
+          //       ),
+          //     ),
+          //     childCount: 20,
+          //   ),
+          // ),
+          SliverGrid(
+            delegate: SliverChildListDelegate([
+              const Icon(Icons.animation),
+              const Icon(Icons.animation),
+              const Icon(Icons.animation),
+            ]),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              childAspectRatio: 1.5,
             ),
+          ),
+          // 剩余空间
+          const SliverFillRemaining(
+            child: Center(child: CircularProgressIndicator()),
           ),
         ],
       ),
